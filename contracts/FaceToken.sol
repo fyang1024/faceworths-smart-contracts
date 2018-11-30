fbafaf699ce12374cbb7cea05846f39acdc68f12
fyang1024 <fyang1024@gmail.com>
2018-12-01 10:23:47 +1100

    mint Face Tokens to a wallet

--- contracts/FaceToken.sol
+++ contracts/FaceToken.sol
@@ -9,8 +9,13 @@ contract FaceToken is StandardToken, Owned {
   string public constant name = "FaceWorths Token";
   string public constant symbol = "FACE";
   uint256 public constant decimals = 6;
+  address public constant initialVault = 0x412DC62C14EE8A473EF42AD8C494DC2E83A99FA1B6;
 
   constructor() public {
     totalSupply_ = (10**8) * 10**decimals; // 100 million
+    balances[initialVault] = totalSupply_;
+    emit Mint(initialVault, balances[initialVault]);
   }
+
+  event Mint(address indexed to, uint256 amount);
 }
