ae51c08b89d07db2a3c8a875369f59bb2099ae68
fyang1024 <fyang1024@gmail.com>
2018-12-04 17:23:21 +1100

    FaceWorthPoll creation test

--- test/FaceWorthPollFactory.test.js
+++ test/FaceWorthPollFactory.test.js
@@ -1,7 +1,8 @@
 const FaceToken = artifacts.require("../contracts/FaceToken.sol");
 const FaceWorthPollFactory = artifacts.require("../contracts/FaceWorthPollFactory.sol");
+const Tronweb = require("tronweb");
 
-contract('FaceWorthPollFactory', async () => {
+contract('FaceWorthPollFactory', async (accounts) => {
 
     let faceToken;
     let factory;
@@ -20,4 +21,21 @@ contract('FaceWorthPollFactory', async () => {
         let faceTokenRewardPool = faceTokenRewardPoolInSun / (10 ** decimals);
         assert.equal(eightyPercentOfTotalSupply, faceTokenRewardPool, "faceTokenRewardPool wasn't 80 percent of FaceToken totalSupply");
     });
+
+    it("FaceWorthPoll contract is created successfully", async () => {
+        let faceHash = Tronweb.sha3("Some face photo", true);
+        let blocksBeforeReveal = 100; // min number of blocks
+        let blocksBeforeEnd = blocksBeforeReveal;
+        let participantsRequired = 3;
+        await factory.deployFaceWorthPoll(faceHash, blocksBeforeReveal, blocksBeforeEnd, participantsRequired);
+        let numberOfPolls = await factory.getNumberOfPolls();
+        assert.equal(numberOfPolls, 1, "Number of polls wasn't 1 after 1 poll is created");
+        let event = factory.FaceWorthPollDeployed();
+        event.watch( async (err, response) => {
+            let result = await factory.verify(response.args.contractAddress);
+            assert.equal(result[0], true, "The result wasn't valid");
+            assert.equal(result[1], accounts[0], "Initiator wasn't set correctly");
+            event.stopWatching();
+        });
+    })
 });
